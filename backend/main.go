4f389a381d2e43c0d8bf26a628495001d62dc69a
ahmad ali <ahmadabdelaziz8@gmail.com>
2026-04-11 20:45:52 +0200

    feat: update image processing function to integrate with Gemma API for diagnosis

--- backend/main.go
+++ backend/main.go
@@ -58,15 +58,56 @@ func (s *Server) GetDiagnostic(ctx context.Context, req *pb.GetDiagnosticRequest
 			}
 		)
 	}
-
 }
 
-// function to send the image to the vision model, and get the diagnosis
-func sendImageToVisionModel(imageBase64 string) (string, error) {
-	// step 1: send the image to the vision model
+// function to send the image to the vision model, and get the diagnosis 
+func sendImageToGemma(imageBase64 string) (string, error) {
+	openRouterApiKey := os.Getenv("OPENROUTER_API_KEY")
+	gemmaUrl := "https://openrouter.ai/api/v1/chat/completions"
+	model := "google/gemini-2.5-flash"
 
+	// payload, set stream to true 
+	payload := map[string]interface{} {
+		"model":model,
+		"stream":true,
+		"messages":[]map[string]interface{}{
+			{
+				"role":"user",
+				"content":[]map[string]interface{}{
+					{
+						"type":"image_url",
+						"image_url":map[string]interface{}{
+							"url":fmt.Sprintf("data:image/jpeg;base64,%s", imageBase64),
+						},
+					},
+				},
+				"system": "You are a radiologist. You are given an x-ray image with or without a clinical history. \n
+				You need to diagnose the patient based on the image and the clinical history. \n
+				provide location of the fracture if any, and the type of fracture if any. \n
+				provide the diagnosis in a json format with the following fields: \n
+				- diagnosis: the diagnosis of the patient \n
+				- location: the location of the fracture if any \n
+				- type: the type of fracture if any \n
+				- severity: the severity of the fracture if any \n
+				- treatment: the treatment plan for the patient \n
+				- prognosis: the prognosis for the patient \n
+				- recommendation: the recommendation for the patient \n
+				- note: any additional notes or comments \n
+			},
+		},
+		"max_tokens":1000,
+		"temperature":0.7,
+		"top_p":1,	
+		"frequency_penalty":0,
+		"presence_penalty":0,
+		"n":1,
+		"stop":[]string{},
+		"stream_options":map[string]interface{}{
+			"include_usage":true,
+		},
+	}
+	
 }
-
 
 func main() {
 	// fmt.Println("Hello, World!")
