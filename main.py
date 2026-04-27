fc324425e98b397ef26dea60b8a533cb23d865e5
ahmad ali <ahmadabdelaziz8@gmail.com>
2026-04-27 21:15:00 +0300

    feat: make the main components

--- main.py
+++ main.py
@@ -1,5 +1,50 @@
+from openai import OpenAI
+from typing import List, Dict
+
+from dotenv import load_dotenv
+import os
+load_dotenv()
+
+# chat history: Global var for now 
+chat_history: List[Dict[str,str]] = [
+    {"""role": "system", "content": "you are helping orthopaedic surgeons to learn for FRCS, 
+        make your response as concise as possible
+    """}
+]
+
+def send_convo_LLM(chat_history:List[Dict[str, str]]) -> str:
+    # MODEL = "gemma-4-26b-a4b-it"
+    MODEL = "gemma-4-31b-it"
+    GEMINI_APU_KEY = os.getenv("GEMINI_API_KEY")
+
+    client = OpenAI(
+        api_key=GEMINI_APU_KEY,
+        base_url="https://generativelanguage.googleapis.com/v1beta/openai/"
+    )
+
+    # write the response function 
+    response = client.chat.completions.create(
+        model = MODEL,
+        messages= chat_history,
+    )
+
+    return response.choices[0].message.content
+
+    
 def main():
-    print("Hello from ortho-pro!")
+    # greet the user 
+    print("\nhello, I am ortho-pro, ask me anything in orthopaedic surgery!")
+    # the agentic loop
+    while True: 
+        # get the user message 
+        message = input("you: ") 
+        
+        # append it to the message hx: 
+        chat_history.append({"role":"user", "content": message})
+        print("wait for the LLM response ....")
+
+        result = send_convo_LLM(chat_history)
+        print(f"\northo-pro:{result}")
 
 
 if __name__ == "__main__":
---
+++ todo.md
@@ -0,0 +1,14 @@
+# TODO
+chatbot: 
+- infinite loop
+- exit with a way I want to use
+- openAI format 
+- using gemma
+
+====
+steps: 
+- the user writes message, 
+- message is appended to the convsersation, then it's being sent to the llm, 
+- add the llm response to it and display it
+
+# DONE
