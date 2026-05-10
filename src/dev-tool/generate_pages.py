8bd5ce3beb9a275b2d16c489425e987849a78c71
ahmad ali <ahmadabdelaziz8@gmail.com>
2026-05-10 22:29:26 +0300

    feat: re-generate the json files, with chunks instead of pages

---
+++ src/dev-tool/generate_pages.py
@@ -0,0 +1,81 @@
+import pymupdf as pm
+import json
+from pathlib import Path
+
+
+# load mcrea
+def load_mcrea():
+    # step 1:
+    MCREA_PATH = "books/McRae's Orthopaedic Trauma andEmergency Fracture_Management.pdf"
+    try:
+        mcrea_doc = pm.open(filename=MCREA_PATH, filetype="pdf")
+        total_pages = mcrea_doc.page_count
+        print(f"total page count is : {total_pages}")
+        print(type(mcrea_doc))
+
+        return mcrea_doc
+
+    except Exception as e:
+        print(e)
+    # loop over the pages,
+
+
+# generate pages
+def generate_pages(mcrea_doc):
+    # loop over pages, and for each page, create a json file, that contains it's the page number and the
+    # paragraphs, each paragraph is separated by /n/n
+    DOC_PATH = Path("/Users/ahmad-ali/repos/ortho-pro/data/pages_data")
+    # create a folder for that path if doesn't exist
+    DOC_PATH.mkdir(parents=True, exist_ok= True)
+
+    page_count = mcrea_doc.page_count
+    for page_index in range(page_count):
+        page_chunks = []
+        page = mcrea_doc[page_index]
+        page_text = page.get_text("text")
+
+        # clean text 
+        page_text = page_text.replace("\n", " ")
+        page_text = page_text.replace("\t", " ")
+        page_text = page_text.replace("\u00ad", " ")
+        words = page_text.split()
+        # chunks variables
+        chunk_no = 1
+        chunk_content = " "
+        chunk_words_count = 0
+        #generate chunkgs foreach 20 words
+        for word_index, word in enumerate(words): 
+            # start_index = word_index
+            # end_index = words.length 
+
+            if chunk_words_count < 20: 
+                chunk_content += word + " "
+                chunk_words_count += 1    
+            else: 
+                chunk = {
+                    "id": chunk_no,
+                    "content": chunk_content,
+                }
+                page_chunks.append(chunk)
+                chunk_no += 1
+                chunk_content = word + " "
+                chunk_words_count = 0
+
+        # create the page object
+        page_content = {"page_number": page_index + 1, "chunks":page_chunks }
+        file_path = DOC_PATH / f"page_{page_index + 1}.json"
+        
+        # open the doc and save the dict there,
+        with open(file_path, "w") as file:
+            # save the page as json
+            json.dump(
+                page_content,
+                file,
+            )
+
+        print(f"page {page_index + 1} has been saved successfully")
+
+
+if __name__ == "__main__":
+    doc = load_mcrea()
+    generate_pages(doc)
--- todo.md
+++ todo.md
@@ -1,24 +1,31 @@
 # TODO
 
-[]
- - read the book 
- - save it into pages 
- - foreach page, save it's contents as paragraphs
- - chunks will be the retrieved units
- - two tools: search (keyword), read (page number as ID)
+## search tool [done] 
+ - params: (query: list of strings)
+ - output: dict, contains page_number, snippet, score 
+
+## read tool 
+ - params: page number: integer
+ - output: dict {key: paragraph_no, value: string contains paragraph's text}
 
 
 # DONE
-[x] chatbot: 
-- infinite loop
+## [x] turn the book into folder of json files
+    - infinite loop
+    - read the book 
+    - save it into json file, each one is a page
+    - foreach page, save it's contents as paragraphs
 
-- exit with a way I want to use
-- openAI format 
-- using gemma
+## [x] chatbot: 
 
-=========
-steps: 
-- the user writes message, 
-- message is appended to the convsersation, then it's being sent to the llm, 
-- add the llm response to it and display it
+
+    - exit with a way I want to use
+    - openAI format 
+    - using gemma
+
+    =========
+    steps: 
+    - the user writes message, 
+    - message is appended to the convsersation, then it's being sent to the llm, 
+    - add the llm response to it and display it
 
